485c61d233afca7790e0d6e372dbbb5c5a02cec6
floppyflop2 <floppy007_flo@hotmail.com>
2018-02-05 17:23:40 +0100

    Modif vers oauth

---
+++ Service/App_Start/WebApiConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+using System.Web.Http.Cors;
+
+namespace Service
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            // Configure Web API to use only bearer token authentication.
+            config.SuppressDefaultHostAuthentication();
+            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            var enableCorsAttribute = new EnableCorsAttribute("*",
+                                               "Origin, Content-Type, Accept, Authorization",
+                                               "GET, PUT, POST, DELETE, OPTIONS");
+            config.EnableCors();
+            
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ TestService/Controllers/DispatchController.cs
@@ -0,0 +1,71 @@
+﻿using Service.Models;
+using Newtonsoft.Json;
+using Operations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Service
+{
+    [Authorize]
+    public class DispatchController : ApiController
+    {
+
+        [HttpGet]
+     //   [Authorize(Roles = "Admin")]
+        [Route("{name}/{id}")]
+        public object DispatchGet(string name, string id)
+        {
+            return name == null ? "Give a name" : Operation.Get(name, id);
+        }
+
+        [HttpGet]
+        [Route("{name}")]
+        public object DispatchGet(string name)
+        {
+            return name == null ? "Give a name" : Operation.Get(name, User.Identity.GetUserId());
+        }
+
+        [HttpPost]
+        [Route("{name}")]
+        public object DispatchPost(RequestModel obj, string name)
+        {
+            return name == null ? "Give a name" : Operation.Add(name, obj.FindCorrectDTO(), User.Identity.GetUserId());
+        }
+
+        
+        [HttpPut]
+        [Route("{name}")]
+        public object DispatchPut(RequestModel obj, string name)
+        {
+            if (name == null)
+                return "Give a name";
+            Operation.Modify(name, obj.FindCorrectDTO(), User.Identity.GetUserId());
+            return "";
+        }
+
+  //      [Authorize(Roles = "Admin")]
+        [HttpDelete]
+        [Route("{name}")]
+        public object DispatchDelete(RequestModel obj, string name)
+        {
+            if  (name == null)
+                return "Give a name";
+            Operation.Remove(name, obj.FindCorrectDTO());
+            return "";
+        }
+        
+
+        [HttpGet]
+        [Route("Admin")]
+        public bool DispatchRole()
+        {
+            return User.IsInRole("Admin");
+        }
+    }
+}
