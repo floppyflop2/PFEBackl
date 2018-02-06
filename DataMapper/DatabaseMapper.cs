2dbcd0a1d39381ccf7e44c1d15ee31b9ad413134
floppyflop2 <floppy007_flo@hotmail.com>
2018-02-06 11:30:25 +0100

    rajouts des mapper pour machines , user et problems

--- DataMapper/DatabaseMapper.cs
+++ DataMapper/DatabaseMapper.cs
@@ -10,8 +10,9 @@ namespace DataMapper
         {
             return new UserDTO()
             {
-                Id = user.UserId,
-                Email = user.UserEmail,
+                UserId = user.UserId,
+                UserEmail = user.UserEmail,
+                AspNetUserId = user.AspNetUserId
             };
         }
         public static List<UserDTO> MapToUserDTO(List<Users> users)
@@ -19,6 +20,47 @@ namespace DataMapper
             List<UserDTO> listOfUsers = new List<UserDTO>();
             users.ForEach(s => listOfUsers.Add(MapToUserDTO(s)));
             return listOfUsers;
+        }
+
+        public static MachinesDTO MapToMachinesDTO(Machines machine)
+        {
+            return new MachinesDTO
+            {
+                MachineId = machine.MachineId,
+                MachineName = machine.MachineName,
+                MacAddress = machine.MacAddress,
+                Comment = machine.Comment,
+                Statut = machine.Statut
+            };
+        }
+
+        public static List<MachinesDTO> MapToMachinesDTO(List<Machines> machines)
+        {
+            List<MachinesDTO> listOfMachines = new List<MachinesDTO>();
+            machines.ForEach(m => listOfMachines.Add(MapToMachinesDTO(m)));
+            return listOfMachines;
+        }
+
+        public static ProblemsDTO MapToProblemsDTO(Problems problem)
+        {
+            return new ProblemsDTO
+            {
+                ProblemId = problem.ProblemId,
+                ProbDescription = problem.ProbDescription,
+                Photo = problem.Photo,
+                MachineId = problem.MachineId,
+                UserId = problem.UserId,
+                DateProb = problem.DateProb,
+                Statut = problem.Statut,
+                isFixed = problem.Fixed
+            };
+        }
+
+        public static List<ProblemsDTO> MapToProblemsDTO(List<Problems> problems)
+        {
+            List<ProblemsDTO> listOfProblems = new List<ProblemsDTO>();
+            problems.ForEach(p => listOfProblems.Add(MapToProblemsDTO(p)));
+            return listOfProblems;
         }
     }
 }
